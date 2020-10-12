3ee42fa2146e8e12c11ff219167b9a3e640cf92a
Dean Wan <deanwan14@gmail.com>
2020-10-13 08:26:23 +1100

    Morning updates, 13th Oct

--- rb101_programming_foundations/notes/lesson_6/tictactoe.rb
+++ rb101_programming_foundations/notes/lesson_6/tictactoe.rb
@@ -7,6 +7,7 @@ def prompt(msg)
 end
 
 def display_board(board)
+  system 'clear'
   puts
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
@@ -43,7 +44,53 @@ def player_places_piece!(board)
   board[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMPUTER_MARKER
+end
+
+def board_full?(board)
+  empty_squares(board).empty?
+end
+
+def someone_won?(board)
+  !!detect_winner(board)
+end
+
+def detect_winner(board)
+  winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + 
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + 
+                  [[1, 3, 9], [3, 5, 7]]
+  winning_combos.each do |combo|
+    if board[combo[0]] == PLAYER_MARKER &&
+      board[combo[1]] == PLAYER_MARKER && 
+      board[combo[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif board[combo[0]] == COMPUTER_MARKER &&
+      board[combo[1]] == COMPUTER_MARKER && 
+      board[combo[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
+end
+
 board = initialise_board
+
+loop do 
+  display_board(board)
+
+  player_places_piece!(board)
+  break if someone_won?(board) || board_full?(board)
+
+  computer_places_piece!(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
 display_board(board)
-player_places_piece!(board)
-display_board(board)
+
+if someone_won?(board)
+  prompt("#{detect_winner(board)} won!")
+else
+  prompt('It\'s a tie!')
+end
