d238410012e35b34b5f7878395ae8923f846c0d4
Dean Wan <deanwan14@gmail.com>
2020-10-17 13:44:29 +1100

    Afternoon updates

--- rb101_programming_foundations/lesson_6/twentyone.rb
+++ rb101_programming_foundations/lesson_6/twentyone.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-byebug'
+
 number_cards = (2..10).to_a.map(&:to_s)
 face_cards = %w(King Queen Jack Ace)
 cards = number_cards + face_cards
@@ -45,8 +48,21 @@ def hit!(shoe)
   shoe.shuffle!.pop
 end
 
+def hand_value(hand)
+  CARDS.values_at(*hand).reduce(:+)
+end
+
+def alternate_player(player)
+  player == 'Player' ? 'Dealer' : 'Player'
+end
+
+def alternate_hand(current_hand, dealer_hand, player_hand)
+  current_hand == player_hand ? dealer_hand : player_hand
+end
+
 def dealer_choice(shoe, dealer_hand)
-  if CARDS.values_at(*dealer_hand).reduce(:+) < 17
+  # pull this out into a value check
+  if hand_value(dealer_hand) < 17
     hit!(shoe)
   end
 end
@@ -67,17 +83,25 @@ def player_turn(player_choice, shoe, player_hand)
   end
 end
 
-def value_check(hand)
+def win_check(hand)
   # How do I account for multiple aces?
-  if CARDS.values_at(*hand).reduce(:+) > 21
-    return 'Bust!'
-  elsif CARDS.values_at(*hand).reduce(:+) == 21
-    return 'Winner!'
+  if hand_value(hand) > 21
+    return 'busts'
+  elsif hand_value(hand) == 21
+    return 'wins'
   end
 end
 
+def end_game_result(player, condition, hand)
+  if condition == 'busts' || condition == 'wins'
+    [player, condition, hand]
+  end
+end
 
-# Main game loop
+def which_state(condition1, condition2)
+  condition1 ? condition1 : condition2
+end
+
 system 'clear'
 welcome
 
@@ -85,14 +109,29 @@ loop do
   shoe = CARDS.keys * 4
   player_hand = initial_hand(shoe)
   dealer_hand = initial_hand(shoe)
+  current_player = 'Dealer'
+  current_hand = dealer_hand
 
   print_initial_hands(player_hand, dealer_hand)
   loop do
     player_turn(player_choice, shoe, player_hand)
-    p player_hand
-    p value_check(player_hand)
+    prompt("Your hand is #{player_hand}")
+    break if end_game_result(alternate_player(current_player), win_check(player_hand), player_hand)
     
     dealer_turn(shoe, dealer_hand)
+    prompt('The dealer hits.')
+    break if end_game_result(alternate_player(current_player), win_check(dealer_hand), dealer_hand)
   end
 
+  state = which_state(end_game_result(alternate_player(current_player), win_check(player_hand), player_hand), end_game_result(alternate_player(current_player), win_check(dealer_hand), dealer_hand))
+  puts
+  prompt("The #{state[0]} #{state[1]} with a hand of #{state[2]}!")
+  puts
+  prompt('Do you want to play again? Please enter \'yes\' or \'no\'.')
+  input = gets.chomp.downcase
+  break unless input.start_with?('y')
+  system 'clear'
 end
+
+puts
+prompt('Thanks for playing Twenty-one!')
