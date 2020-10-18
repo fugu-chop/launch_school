3f4366bd378a6376cac959417ac576dc5fe29f07
Dean Wan <deanwan14@gmail.com>
2020-10-18 11:26:27 +1100

    Morning updates, 18th Oct

--- rb101_programming_foundations/lesson_6/twentyone.rb
+++ rb101_programming_foundations/lesson_6/twentyone.rb
@@ -3,6 +3,10 @@ face_cards = %w(King Queen Jack Ace)
 cards = number_cards + face_cards
 values = (2..10).to_a + [10, 10, 10, 11]
 CARDS = Hash[cards.zip(values)]
+DEALER_STAY = 17
+BLACKJACK = 21
+SUITS = 4
+STARTING_HAND_SIZE = 2
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -14,13 +18,12 @@ def welcome
   prompt("The goal of this game is to draw to 21.")
   prompt("If you go over 21, you bust.")
   prompt("Dealer hits to 17 exclusive.")
-  prompt("Dealer wins on draws.")
   prompt("No splitting or doubling down.")
   puts
 end
 
 def initial_hand!(shoe)
-  shoe.shuffle!.pop(2)
+  shoe.shuffle!.pop(STARTING_HAND_SIZE)
 end
 
 def print_initial_hands(player_hand, dealer_hand)
@@ -46,81 +49,118 @@ def hit!(shoe)
 end
 
 def hand_value(hand)
-  CARDS.values_at(*hand).reduce(:+)
+  final_value = CARDS.values_at(*hand).reduce(:+)
+  counter = 0
+  if hand.count('Ace') > 0 && final_value > BLACKJACK
+    while final_value > BLACKJACK && counter != hand.count('Ace')
+      final_value -= 10
+      counter += 1
+    end
+  end
+  final_value
 end
 
-def dealer_choice(dealer_hand)
-  hand_value(dealer_hand) < 17
+def dealer_hit?(dealer_hand)
+  hand_value(dealer_hand) < DEALER_STAY
 end
 
 def dealer_turn(shoe, dealer_hand)
-  if dealer_choice(dealer_hand)
+  if dealer_hit?(dealer_hand)
     dealer_hand << hit!(shoe)
   else
     dealer_hand
   end
 end
 
-def player_win_or_bust(hand)
-  # How do I account for multiple aces?
-  if hand_value(hand) > 21
+def win_or_bust(hand)
+  if hand_value(hand) > BLACKJACK
     'busts'
-  elsif hand_value(hand) == 21
+  elsif hand_value(hand) == BLACKJACK
     'wins'
   end
 end
 
-def win_or_bust_result(player, condition, hand)
-  if condition == 'busts' || condition == 'wins'
-    [player, condition, hand]
+def determine_winner(player_hand, dealer_hand)
+  if hand_value(dealer_hand) > BLACKJACK ||
+     hand_value(player_hand) == BLACKJACK ||
+     (hand_value(player_hand) < BLACKJACK &&
+      hand_value(player_hand) > hand_value(dealer_hand))
+    ['player', player_hand]
+  else
+    ['dealer', dealer_hand]
   end
+end
+
+def alternate_player(player)
+  player == 'player' ? 'dealer' : 'player'
+end
+
+def display_winner(player_hand, dealer_hand)
+  player = determine_winner(player_hand, dealer_hand)[0]
+  hand = determine_winner(player_hand, dealer_hand)[1]
+  if hand_value(player_hand) > BLACKJACK || hand_value(dealer_hand) > BLACKJACK
+    prompt("The #{alternate_player(player)} busts!")
+  end
+  if hand_value(player_hand) == hand_value(dealer_hand)
+    prompt("It's a tie!")
+  else
+    prompt("The #{player} wins with #{hand}, totalling #{hand_value(hand)}!")
+  end
+  puts
+end
+
+def display_hand(hand, player)
+  prompt("The #{player}'s hand is #{hand}.")
+  prompt("The #{player}'s hand value is #{hand_value(hand)}.")
 end
 
 system 'clear'
 welcome
 
 loop do
-  shoe = CARDS.keys * 4
+  shoe = CARDS.keys * SUITS
   player_hand = initial_hand!(shoe)
   dealer_hand = initial_hand!(shoe)
 
   print_initial_hands(player_hand, dealer_hand)
   loop do
+    player = 'player'
+    break if win_or_bust(player_hand)
     if player_choice == 'hit'
+      prompt('You\'ve chosen to hit.')
       player_hand << hit!(shoe)
+      display_hand(player_hand, player)
       puts
-      prompt("Your hand is #{player_hand}")
-      break if player_win_or_bust(player_hand)
+      break if win_or_bust(player_hand)
     else
-      prompt("Your hand is #{player_hand}")
+      prompt('You\'ve chosen to stay.')
+      display_hand(player_hand, player)
       puts
+      prompt('It\'s the dealer\'s turn!')
       break
     end
   end
 
-  break if player_win_or_bust(player_hand)
-  prompt('It\'s the dealer\'s turn!')
   puts
   loop do
-    prompt("The dealer's hand is #{dealer_hand}")
-    break if !dealer_choice(dealer_hand)
+    player = 'dealer'
+    break if win_or_bust(player_hand)
+    display_hand(dealer_hand, player)
+    puts
+    break if !dealer_hit?(dealer_hand)
     dealer_turn(shoe, dealer_hand)
-    prompt('The dealer hits.')
+    prompt("The #{player} hits.")
     puts
   end
 
-  puts
+  display_winner(player_hand, dealer_hand)
+
   prompt('Do you want to play again? Please enter \'yes\' or \'no\'.')
   input = gets.chomp.downcase
   break unless input.start_with?('y')
+
   system 'clear'
 end
 
 puts
 prompt('Thanks for playing Twenty-one!')
-
-=begin
-To DOs:
-- win conditions
-- Ace logic
-=end
