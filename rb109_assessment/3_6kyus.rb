38ae7ee74f8150e101e4b71a41d0fa03d61e28c7
Dean Wan <deanwan14@gmail.com>
2020-11-23 08:16:18 +1100

    Morning updates, 23rd Nov

--- rb109_assessment/3_6kyus.rb
+++ rb109_assessment/3_6kyus.rb
@@ -79,3 +79,96 @@ def solve(arr)
   # The negative sign allows you to reverse!
   arr.sort_by { |element| [-arr.count(element), element] }
 end
+
+# 3) Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd". Your task is to process a string with "#" symbols.
+=begin
+clean_string('abc#d##c') == "ac"
+clean_string('abc####d##c#') == "" 
+clean_string("#######") == ""
+clean_string("") == ""
+=end
+def clean_string(string)
+  keep_letters = []
+  string.each_char do |char|
+    char == '#' ? keep_letters.pop : keep_letters << char
+  end
+  keep_letters.join
+end
+
+# Using each_with_object
+def clean_string(str)
+  str.chars.each_with_object([]) { |ch, o| ch == "#" ? o.pop : o << ch }.join
+end
+
+# 4) Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime. Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself. You can assume you will be given an integer input.You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+=begin
+is_prime(1) == false
+is_prime(2) == true
+is_prime(-1) == false
+=end
+# This works, but times out on Codewars' weaksauce computers
+def is_prime?(integer)
+  return false if integer <= 1
+  (2..integer).each_with_object([]) do |num, arr|
+    arr << num if integer % num == 0
+    return false if arr.length > 1
+  end.length == 1
+end
+
+# Optimised solution
+def is_prime?(num)
+  # I am not sure how 'clean' this solution is, tbh. Not every integer will have a clean square root...
+  num < 2 ? false : (2..Math.sqrt(num)).none?{ |i| num % i == 0 }
+end
+
+# 5) You will create the classic Pascal's triangle. Your function will be passed the depth of the triangle and you code has to return the corresponding pascal triangle up to that depth. The triangle should be returned as a nested array.
+=begin
+pascal(5) # should return [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+=end
+def pascal(rows)
+  push_array = [[1]]
+  counter = 1
+  loop do
+    break if counter >= rows
+    new_arr = []
+    push_array.last.each_cons(2) { |a, b| new_arr << [a, b].reduce(:+) }
+    new_arr.insert(0, 1)
+    new_arr.insert(-1, 1)
+    push_array << new_arr
+    counter +=1
+  end
+  push_array
+end
+
+# 6) Your task, is to create NxN multiplication table, of size provided in parameter. 
+=begin
+multiplication_table(3) == [[1,2,3],[2,4,6],[3,6,9]]
+
+1 2 3
+2 4 6
+3 6 9
+
+1 2 3 4
+2 4 6 8
+3 6 9 12
+4 8 12 16
+=end
+def multiplication_table(size)
+  # size defines how many subarrays within our array
+  # The first element of the each subarray increments by 1
+  # The second element increments by 2
+  # The third element increments by 3
+  # And so on...
+
+  # Create a blank array for us to push our sub arrays into (push_array)
+  # Create a looping construct
+  # Break our loop when push_array.length == size (put at start of loop)
+    # When we create our first element, we can use (1..size).to_a
+    # Push this to our push_array
+    # Create a blank array []
+    # we want to take the last element of our push_array (potentially use each_with_object), with index
+      # [1,2,3]
+      # [] << element + index + 1
+    # Push the blank array to our push_array
+  # Return the push_array
+end
