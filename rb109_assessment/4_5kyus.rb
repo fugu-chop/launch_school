261f963fc05e47112940fa76d160004e3ec4b89b
Dean Wan <deanwan14@gmail.com>
2020-12-10 12:35:45 +1100

    Morning updates, 10th Dec

--- rb109_assessment/4_5kyus.rb
+++ rb109_assessment/4_5kyus.rb
@@ -982,4 +982,50 @@ def search_substr(full_text, search_text, allow_overlap = true)
   match_count
 end
 
-# 36) 
+# 36) For each number in strng calculate its "weight" and then find two numbers of strng that have, the smallest difference of weights ie that are the closest, with the smallest weights, and with the smallest indices (or ranks, numbered from 0) in strng. The output should be an array of two arrays, each subarray in the following format: [number-weight, index in strng of the corresponding number, original corresponding number instrng].
+=begin
+Read here: https://www.codewars.com/kata/5868b2de442e3fb2bb000119
+
+testing(closest(""), [])
+closest("456899 50 11992 176 272293 163 389128 96 290193 85 52") == [[13, 9, 85], [14, 3, 176]]
+closest("239382 162 254765 182 485944 134 468751 62 49780 108 54") == [[8, 5, 134], [8, 7, 62]]
+closest("241259 154 155206 194 180502 147 300751 200 406683 37 57") == [[10, 1, 154], [10, 9, 37]]
+closest("89998 187 126159 175 338292 89 39962 145 394230 167 1") == [[13, 3, 175], [14, 9, 167]]
+closest("462835 148 467467 128 183193 139 220167 116 263183 41 52") == [[13, 1, 148], [13, 5, 139]]
+closest("403749 18 278325 97 304194 119 58359 165 144403 128 38") == [[11, 5, 119], [11, 9, 128]]
+closest("28706 196 419018 130 49183 124 421208 174 404307 60 24") == [[6, 9, 60], [6, 10, 24]]
+closest("189437 110 263080 175 55764 13 257647 53 486111 27 66") == [[8, 7, 53], [9, 9, 27]]
+closest("79257 160 44641 146 386224 147 313622 117 259947 155 58") == [[11, 3, 146], [11, 9, 155]]
+closest("315411 165 53195 87 318638 107 416122 121 375312 193 59") == [[15, 0, 315411], [15, 3, 87]]
+=end
+def closest(string)
+  return [] if string.length == 0
+
+  full_arr = string.split.map.with_index do |num, idx|
+    [num.chars.map { |char| char.to_i }.reduce(:+), idx, num.to_i]
+  end.sort_by { |a, b, _| [a, b] }
+
+  sorted_arr = full_arr.each_cons(2).map do |subarr1, subarr2|
+    [subarr2[0] - subarr1[0], subarr1, subarr2]
+  end.sort { |element| element[0] }
+
+  sorted_arr.min[1..-1]
+end
+
+# 37) In some countries of former Soviet Union there was a belief about lucky tickets. A transport ticket of any sort was believed to posess luck if sum of digits on the left half of its number was equal to the sum of digits on the right half. Your task is to write a funtion luck_check(str), which returns true/True if argument is string decimal representation of a lucky ticket number, or false/False for all other numbers. It should throw errors for empty strings or strings which don't represent a decimal number.
+=begin
+003111    #             3 = 1 + 1 + 1
+813372    #     8 + 1 + 3 = 3 + 7 + 2
+17935     #         1 + 7 = 3 + 5  // if the length is odd, you should ignore the middle number when adding the halves.
+56328116  # 5 + 6 + 3 + 2 = 8 + 1 + 1 + 6
+=end
+def luck_check (str)
+  if str.length.odd?
+    first_half, second_half = str[0...str.length/2], str[(str.length/2)+1..-1]
+  else
+    first_half, second_half = str[0...str.length/2], str[str.length/2..-1]
+  end
+
+  first_half.to_i.digits.sum == second_half.to_i.digits.sum
+end
+
