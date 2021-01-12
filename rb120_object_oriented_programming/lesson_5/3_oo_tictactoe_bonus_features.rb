7abc989bc36eb130ef19cefee7285dbd308d635c
Dean Wan <deanwan14@gmail.com>
2021-01-13 08:47:43 +1100

    Morning update, 13th Jan

--- rb120_object_oriented_programming/lesson_5/3_oo_tictactoe_bonus_features.rb
+++ rb120_object_oriented_programming/lesson_5/3_oo_tictactoe_bonus_features.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
@@ -62,52 +60,12 @@ class Board
     @squares[num].marker = marker
   end
 
-  # While these are duplicate methods, we need them to run independently
-  # If we put player and computer win conditions together,
-  # we run in to the edge case where
-  # the order of iterating through WINNING_LINES begins to matter
-  # since a player_imminent_win might be assessed earlier
-  # than a computer_imminent win.
-  def computer_defense(human_marker)
-    WINNING_LINES.each do |line|
-      combo = @squares.values_at(*line)
-      if player_imminent_win?(combo, human_marker)
-        return @squares.select do |key, value|
-          line.include?(key) && value.marker == Square::INITIAL_MARKER
-        end.keys.first
-      end
-    end
-    nil
-  end
-
-  def computer_offense(computer_marker)
-    WINNING_LINES.each do |line|
-      combo = @squares.values_at(*line)
-      if computer_imminent_win?(combo, computer_marker)
-        return @squares.select do |key, value|
-          line.include?(key) && value.marker == Square::INITIAL_MARKER
-        end.keys.first
-      end
-    end
-    nil
-  end
-
   private
 
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
     return false if markers.size != 3
     markers.uniq.size == 1
-  end
-
-  def player_imminent_win?(combo, human_marker)
-    combo.collect(&:marker).count(human_marker) == 2 &&
-      combo.collect(&:marker).count(Square::INITIAL_MARKER) == 1
-  end
-
-  def computer_imminent_win?(combo, computer_marker)
-    combo.collect(&:marker).count(computer_marker) == 2 &&
-      combo.collect(&:marker).count(Square::INITIAL_MARKER) == 1
   end
 end
 
@@ -134,7 +92,7 @@ class Square
 end
 
 class Player
-  attr_accessor :score, :marker
+  attr_accessor :score, :marker, :name
 
   def initialize
     @score = 0
@@ -142,16 +100,83 @@ class Player
 end
 
 class Computer < Player
-  def initialize(marker)
+  MARKER = 'O'
+  def initialize
     super()
-    @marker = marker
+    @marker = MARKER
+    set_name
+  end
+
+  def move(board, human_marker)
+    board[computer_selection(board, human_marker)] = marker
+  end
+
+  private
+
+  def set_name
+    self.name = ['Bender', 'R2D2', 'T-800', 'Optimus Prime', 'Sonny'].sample
+  end
+
+  def computer_defense(board, human_marker)
+    Board::WINNING_LINES.each do |line|
+      combo = board.squares.values_at(*line)
+      if imminent_win?(combo, human_marker)
+        return board.squares.select do |key, value|
+          line.include?(key) && value.marker == Square::INITIAL_MARKER
+        end.keys.first
+      end
+    end
+    nil
+  end
+
+  def computer_offense(board)
+    Board::WINNING_LINES.each do |line|
+      combo = board.squares.values_at(*line)
+      if imminent_win?(combo, marker)
+        return board.squares.select do |key, value|
+          line.include?(key) && value.marker == Square::INITIAL_MARKER
+        end.keys.first
+      end
+    end
+    nil
+  end
+
+  def imminent_win?(combo, marker)
+    combo.collect(&:marker).count(marker) == 2 &&
+      combo.collect(&:marker).count(Square::INITIAL_MARKER) == 1
+  end
+
+  def computer_selection(board, human_marker)
+    if computer_offense(board)
+      computer_offense(board)
+    elsif computer_defense(board, human_marker)
+      computer_defense(board, human_marker)
+    elsif board.unmarked_keys.include?(5)
+      5
+    else
+      board.unmarked_keys.sample
+    end
   end
 end
 
 class Human < Player
   def initialize
     super()
+    set_name
     choose_marker
+  end
+
+  def move(board, joinor)
+    square = nil
+    puts "Please select a square: #{joinor}."
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts
+      puts "Sorry, that's not a valid choice. Please try again!"
+    end
+
+    board[square] = marker
   end
 
   private
@@ -159,18 +184,37 @@ class Human < Player
   def choose_marker
     answer = nil
     loop do
-      puts "Please choose a marker - you can select any alphanumeric character."
+      puts
+      puts "Please choose a marker - you can select a character aside from 'O'."
       answer = gets.chomp
-      break if !answer.empty?
-      puts "That appears to be an empty input. Please try again!"
+      break if answer_validation(answer)
+      puts "Invalid marker. Please try again!"
       puts
     end
-    @marker = answer
+    self.marker = answer
+  end
+
+  def answer_validation(answer)
+    !answer.empty? &&
+      answer.upcase != Computer::MARKER &&
+      answer != ' ' &&
+      answer.length == 1
+  end
+
+  def set_name
+    answer = nil
+    loop do
+      puts "Please enter your name!"
+      answer = gets.chomp
+      break if !answer.empty?
+      puts "Empty input detected. Please try again"
+      puts
+    end
+    self.name = answer
   end
 end
 
 class TTTGame
-  COMPUTER_MARKER = 'O'
   FIRST_MOVER = 'choose'
   WIN_TOTAL = 3
 
@@ -179,9 +223,11 @@ class TTTGame
   def initialize
     clear
     display_welcome_message
+    display_rules
     @board = Board.new
+    @computer = Computer.new
+    display_opponent
     @human = Human.new
-    @computer = Computer.new(COMPUTER_MARKER)
     @current_turn = who_moves_first
   end
 
@@ -195,6 +241,11 @@ class TTTGame
 
   def clear
     system 'clear'
+  end
+
+  def display_opponent
+    puts "You'll be playing #{computer.name} today."
+    puts
   end
 
   def display_welcome_message
@@ -216,36 +267,6 @@ class TTTGame
     else
       arr[-1]
     end
-  end
-
-  def human_moves
-    square = nil
-    puts "Please select a square: #{joinor}."
-    loop do
-      square = gets.chomp.to_i
-      break if board.unmarked_keys.include?(square)
-      puts
-      puts "Sorry, that's not a valid choice. Please try again!"
-    end
-
-    board[square] = human.marker
-  end
-
-  def computer_selection(human_marker = human.marker,
-                         computer_marker = computer.marker)
-    if board.computer_offense(computer_marker)
-      board.computer_offense(computer_marker)
-    elsif board.computer_defense(human_marker)
-      board.computer_defense(human_marker)
-    elsif board.unmarked_keys.include?(5)
-      5
-    else
-      board.unmarked_keys.sample
-    end
-  end
-
-  def computer_moves
-    board[computer_selection] = computer.marker
   end
 
   def human_turn?
@@ -280,16 +301,25 @@ class TTTGame
 
   def current_player_moves
     if human_turn?
-      human_moves
+      human.move(board, joinor)
       @current_turn = computer.marker
     else
-      computer_moves
+      computer.move(board, human.marker)
       @current_turn = human.marker
     end
   end
 
+  def display_rules
+    puts <<~MSG
+    The goal is to mark 3 squares, either vertically, horizontally, or diagonally, consecutively.
+    The first player to win #{WIN_TOTAL} rounds, wins the game.
+    Good luck!
+    MSG
+    puts
+  end
+
   def display_board
-    puts "You're marking #{human.marker}, the computer is #{computer.marker}."
+    puts "You're '#{human.marker}', #{computer.name} is '#{computer.marker}'."
     puts
     board.draw
     puts
@@ -304,9 +334,9 @@ class TTTGame
     clear_screen_and_display_board
     case board.winning_marker
     when human.marker
-      puts "You won!"
+      puts "#{human.name} won!"
     when computer.marker
-      puts "The computer won!"
+      puts "#{computer.name} won!"
     else
       puts "It's a tie!"
     end
@@ -328,7 +358,7 @@ class TTTGame
 
   def overall_winner
     return unless overall_winner?
-    human.score > computer.score ? "player" : "computer"
+    human.score > computer.score ? (human.name).to_s : (computer.name).to_s
   end
 
   def display_overall_winner
@@ -337,9 +367,9 @@ class TTTGame
   end
 
   def display_score
-    puts "Total wins:
-    Player: #{human.score}
-    Computer: #{computer.score}"
+    puts "Total wins:"
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
     puts
   end
 
@@ -362,9 +392,7 @@ class TTTGame
 
   def reset
     board.reset
-    # I've decided to give the player to choose on subsequent rounds
-    # even if the constant is set to player/computer
-    @current_turn = choose_first_to_move
+    @current_turn = who_moves_first
     reset_scores
     clear
   end
@@ -379,6 +407,7 @@ class TTTGame
     when 'computer' then computer.marker
     when 'player' then human.marker
     else
+      # perhaps have this execute conditinally in the initialize method?
       choose_first_to_move
     end
   end
@@ -386,6 +415,7 @@ class TTTGame
   def choose_first_to_move
     answer = nil
     loop do
+      puts
       puts "Who should go first? Please answer [p]layer or [c]omputer."
       answer = gets.chomp.downcase
       break if ['p', 'c', 'computer', 'player'].include?(answer)
@@ -393,6 +423,7 @@ class TTTGame
       puts
     end
 
+    # if this method is executed, we want to write to write this to @current_turn
     ['p', 'player'].include?(answer) ? human.marker : computer.marker
   end
 
