de6614337ee7d6613ee3bcbfb0705c7ec6168277
Dean Wan <deanwan14@gmail.com>
2021-03-15 19:49:51 +1100

    Evening update, 15th Mar

--- exercises/5_ruby_challenges/2_medium/3_clock.rb
+++ exercises/5_ruby_challenges/2_medium/3_clock.rb
@@ -5,18 +5,46 @@ class Clock
   end
 
   def self.at(hour, minute=0)
+    # We can initialise a new object within the class method
     new(hour, minute)
   end
 
   def to_s
+    # Each % represents a variable, 0 represents the string to be appended, 2 represents the total number of digits
+    format('%02d:%02d', @hour, @minute)
   end
 
-  def +(minutes)
+  def +(add_minutes)
+    hours, minutes = add_minutes.divmod(60)
+
+    # Refactor these!
+
+    # Minute check
+    if minutes + @minute > 59
+      hours += (minutes + @minute).divmod(60).first
+      minutes += (minutes + @minute).divmod(60).last
+    end
+    @minute += minutes
+
+    # Day check
+    if hours + @hour > 23
+      days, additional_hours = hours.divmod(24)
+      @hour += additional_hours 
+    else
+      @hour += hours
+    end
+    
+    # Manually override day overflow
+    @hour >= 24 ? @hour -= 24 : @hour
+
+    # Need to return a string output as a result
+    self.to_s
   end
 
   def -(minutes)
   end
 
   def ==(clock)
+    self.to_s == clock.to_s
   end
 end
--- exercises/5_ruby_challenges/2_medium/3_clock_test.rb
+++ exercises/5_ruby_challenges/2_medium/3_clock_test.rb
@@ -12,7 +12,6 @@ class ClockTest < Minitest::Test
   end
 
   def test_add_a_few_minutes
-    skip
     clock = Clock.at(10) + 3
     assert_equal '10:03', clock.to_s
   end
@@ -24,19 +23,16 @@ class ClockTest < Minitest::Test
   end
 
   def test_add_over_an_hour
-    skip
     clock = Clock.at(10) + 61
     assert_equal '11:01', clock.to_s
   end
 
   def test_wrap_around_at_midnight
-    skip
     clock = Clock.at(23, 30) + 60
     assert_equal '00:30', clock.to_s
   end
 
   def test_add_more_than_a_day
-    skip
     clock = Clock.at(10) + 3061
     assert_equal '13:01', clock.to_s
   end
@@ -66,14 +62,12 @@ class ClockTest < Minitest::Test
   end
 
   def test_equivalent_clocks
-    skip
     clock1 = Clock.at(15, 37)
     clock2 = Clock.at(15, 37)
     assert_equal clock1, clock2
   end
 
   def test_inequivalent_clocks
-    skip
     clock1 = Clock.at(15, 37)
     clock2 = Clock.at(15, 36)
     clock3 = Clock.at(14, 37)
