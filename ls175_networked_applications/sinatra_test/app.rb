0d6f2f22a1ccb595ddba6275535dd66df5e1b1c2
Dean Wan <deanwan14@gmail.com>
2021-05-14 16:35:37 +1000

    Afternoon updates, 14th May

--- ls175_networked_applications/sinatra_test/app.rb
+++ ls175_networked_applications/sinatra_test/app.rb
@@ -3,6 +3,10 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  @files = Dir.glob("public/*")
+  @files = Dir.glob("public/*").map {|file| File.basename(file) }.sort
+  # params is a hash that Sinatra makes available for you in your route blocks, and it will automatically include relevant data from the request.
+  # It also contains any data sent from HTML forms as part of the HTTP request as well as any query params that can be part of the URL.
+  # We can see what it contains through params.inspect
+  @files.reverse! if params[:sort] == "desc"
   erb(:home)
 end
